a295b174ab341e75890f9abff849f7afacfbe2a0
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-04-15 20:35:15 +0900

    * * *

--- Assets/Scripts/DropOnDestroy.cs
+++ Assets/Scripts/DropOnDestroy.cs
@@ -5,18 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class DropOnDestroy : BehaviourOnDestroy
 {
-    public EnduranceBody prefabBody;
+    public EnduranceBody prefabItem;
     public ParticleSystem prefabSmoke;
 
     protected override void DoOnDestroy()
     {
         //drop item
         Builder.Block(
-            prefabBody,
-            transform.position + Quaternion.Euler(0f, 0f, 90f * i) * scale,
+            prefabItem,
+            transform.position,
             transform.rotation);
-
-        if(prefabSmoke != null) Builder.Effecter(prefabSmoke, transform);
     }
 
     private Vector2 SpriteSize()
