b49ebf5fe7816ffee7bcbd40bae2eed8969458d8
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-04-26 08:41:12 +0900

    Background

---
+++ Assets/Background.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // スクロールするスピード
+    public GameObject target;
+    public Vector2 scale;
+    public bool X = true, Y = true;
+    private Vector3 offset;
+    [Range(0, 1)]
+    public float depth = 0.1f;
+
+    private void Start()
+    {
+        offset = target.transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 delta = target.transform.position - offset;
+
+        Vector2 repeat = new Vector2(
+            (X) ? Mathf.Repeat(delta.x * depth / scale.x * 100, 1) : 0f,
+            (Y) ? Mathf.Repeat(delta.y * depth / scale.y * 100, 1) : 0f
+            );
+
+        // マテリアルにオフセットを設定する
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", repeat);
+    }
+}
--- Assets/Scripts/Util/StalkerBehaviour.cs
+++ Assets/Scripts/Util/StalkerBehaviour.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class StalkerBehaviour : MonoBehaviour
 {
     public GameObject target;
-    [SerializeField]
+    [SerializeField][Range(0f, 1f)]
     private float stalkingRate = 1f;
+    public bool X = true, Y = true;
     private Vector3 offset;
 
     void Start () 
@@ -18,7 +19,14 @@ public class StalkerBehaviour : MonoBehaviour
     {
         if (target == null) return;
 
-        transform.position += (target.transform.position + offset - transform.position) * stalkingRate;
+        Vector3 position = 
+                (target.transform.position + offset - transform.position) * stalkingRate;
+
+        if (!X) position.x = 0f;
+        if (!Y) position.y = 0f;
+
+        transform.position += position;
+
         // = target + offset
     }
 }
