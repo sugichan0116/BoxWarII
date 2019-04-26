d8c76d0565880a7fafeacb9d408856832d6029c8
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-04-27 01:51:31 +0900

    clean

---
+++ Assets/Scripts/UI/EventRegion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EventRegion : MonoBehaviour
+{
+    public TweetBox tweetPrefab;
+    public string Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.gameObject.tag != "Player") return;
+
+        TweetBox tweet = Builder.TweetBox(tweetPrefab, transform);
+        tweet.text = Text;
+    }
+}
---
+++ Assets/Scripts/UI/TweetGroup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TweetGroup : MonoBehaviour
+{
+    [SerializeField]
+    public List<string> scripts;
+    [SerializeField]
+    public List<EventRegion> list;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            list[i].Text = scripts[i];
+        }
+    }
+
+}
