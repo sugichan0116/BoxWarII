e6e2c45de2e19f49216adf1cf76965062f72b205
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2019-04-30 09:03:28 +0900

    CameraWork, Effector Experiment

---
+++ Assets/CameraWork.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWork : MonoBehaviour
+{
+    public GameObject target;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+        
+        Vector3 look = Vector3.zero;
+        float height = Input.mousePosition.y / Screen.height;
+        look.y = Mathf.Lerp(-1, 2, height);
+
+        transform.position = target.transform.position + offset + look;
+
+        // = target + offset
+    }
+}
